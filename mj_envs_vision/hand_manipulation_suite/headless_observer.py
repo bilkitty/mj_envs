5cfcdbc6799d757b9b6e3f92dae853d06331f26f
bilkitty <githinjibcw@hotmail.com>
2022-10-27 15:48:36 -0400

    handle zoom in headless observer

--- mj_envs_vision/hand_manipulation_suite/headless_observer.py
+++ mj_envs_vision/hand_manipulation_suite/headless_observer.py
@@ -1,12 +1,17 @@
 import numpy as np
 from gym import utils
+import torch
+import torchvision
 #from contacts import _contact_data
 
 class HeadlessObserver(utils.EzPickle):
+
     def __init__(self, sim, bid: int):
         self.sim = sim
         self.obj_bid = bid
         self.contact_type = 'none'
+        self._resize = torchvision.transforms.Resize((64, 64))
+        self._center_crop = torchvision.transforms.CenterCrop((128, 128))
         utils.EzPickle.__init__(self)
 
         # setup rendering
@@ -28,9 +33,17 @@ class HeadlessObserver(utils.EzPickle):
 
     def render(self, *args, **kwargs):
         # /opt/anaconda3/envs/planet-mjenv/lib/python3.9/site-packages/mujoco_py/mjviewer.py
+        should_resize = 'enable_resize' in args[1].keys() and args[1]['enable_resize']
         image = self.sim.render(640, 480)
-        image = image[::-1, :, :] # Rendered images are upside-down.
-        return np.random.randint((640, 480, 3)) if image is None else image
+        if image is None:
+            return np.random.randint((64, 64, 3)) if should_resize else np.random.randint((128, 128, 3))
+        image = image[::-1, :, :] # rendered images are upside-down
+        image = image.transpose(2, 0, 1).copy()  # put channel first
+        image = self._center_crop(torch.tensor(image, dtype=torch.float32))     # crop
+        if should_resize:
+            image = self._resize(image)
+        # TODO: fix these transforms
+        return image / 255  # normalise + batch dimension
 
 
     def contact_type(self, contact_type: str):
