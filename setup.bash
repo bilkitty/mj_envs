cfb38b382596d8810b6c6b3888543de3be3b4851
bilkitty <githinjibcw@hotmail.com>
2022-11-07 14:29:05 -0500

    ref

--- setup.bash
+++ setup.bash
@@ -1,3 +1,4 @@
 export PYTHONPATH="/home/bilkit/Workspace/mj_envs_vision/mj_envs_vision/:$PYTHONPATH"
+export PYTHONPATH="/home/bilkit/Workspace/PlaNet/:$PYTHONPATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so
