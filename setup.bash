1f3e17401b66bbe568bab469ddde3272b9d374f3
bilkitty <githinjibcw@hotmail.com>
2022-12-19 10:31:53 -0500

    update

--- setup.bash
+++ setup.bash
@@ -1,4 +1,6 @@
-export PYTHONPATH="/home/bilkit/Workspace/mj_envs_vision/mj_envs_vision/:$PYTHONPATH"
-export PYTHONPATH="/home/bilkit/Workspace/PlaNet/:$PYTHONPATH"
+export PYTHONPATH="$(pwd)/mj_envs_vision/:$PYTHONPATH"
+export PYTHONPATH="$(pwd)/dependencies/PlaNet/:$PYTHONPATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
+# NOTE: depending on the machine, use 'unset LD_PRELOAD' if 
+#       GL missing error occurs.
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so
