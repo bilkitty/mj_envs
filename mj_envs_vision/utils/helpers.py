acf8fa192c58b4f858b7ca25de49d4cdade9d934
bilkitty <githinjibcw@hotmail.com>
2023-02-06 15:13:23 -0500

    fix color shift in gifs

--- mj_envs_vision/utils/helpers.py
+++ mj_envs_vision/utils/helpers.py
@@ -11,6 +11,9 @@ from mj_envs_vision.utils.wrappers import CustomObservationWrapper
 from mj_envs_vision.utils.wrappers import CustomPixelObservationWrapper
 
 
+GIF_DURATION = 15
+
+
 class Metrics:
   def __init__(self):
     self.total_return = list()
@@ -71,18 +74,19 @@ def expand(x_flat, sh):
   return x_flat.view(sh[0], sh[1], *x_flat.shape[1:])
 
 def to_image_frame(obs: np.ndarray):
-  """ converts image observation with pels in [-0.5, 0.5] to image with pels in [0, 255] """
+  """ converts image (used as model input) with pels in [-0.5, 0.5] to image with pels in [0, 255] """
   return (255 * (obs.transpose((1, 2, 0)) + 0.5)).astype('uint8')
 
 def to_input_obs(frame: np.ndarray):
-  """ converts image with pels in [0, 255] to image observation with pels in [-0.5, 0.5] """
+  """ converts image with pels in [0, 255] to image observation with pels in [-0.5, 0.5] for model input """
   return (frame.transpose((2, 0, 1)) / 255 - 0.5).astype('float')
 
-def save_as_gif(frames: List[np.ndarray], gif_path: str):
+def save_as_gif(frames: List[np.ndarray], gif_path: str, is_obs: bool=False):
   pils = list()
   for frame in frames:
-    pils.append(Image.fromarray(to_image_frame(frame)))
-  pils[0].save(gif_path, append_images=pils, save_all=True, optimize=False, loop=True, duration=len(pils) / 10)
+    frame = to_image_frame(frame) if is_obs else frame.astype('uint8')
+    pils.append(Image.fromarray(frame))
+  pils[0].save(gif_path, append_images=pils, save_all=True, optimize=False, loop=True, duration=GIF_DURATION)
 
 def plot_rewards(rewards: List[Tuple], yaxis_label="total reward"):
   fig, ax = plt.subplots(1, 1, figsize=(10, 5))
@@ -109,9 +113,7 @@ def plot_rewards(rewards: List[Tuple], yaxis_label="total reward"):
 
 def visualise_batch_from_experience(id, config, experience, out_dir):
   batch = experience.sample(min(config.batch_size, experience.idx - 1), min(config.chunk_size, experience.idx - 1))
-  x = experience.observations.shape[1:]
   save_as_gif(batch[0].reshape(-1, *experience.observations.shape[1:]).cpu().numpy(), os.path.join(out_dir, f'experience_{id}.gif'))
 
 def visualise_trajectory(id, trajectory: List, out_dir):
-  # TODO: fix color (shift? unnormalise?)
-  save_as_gif([x[0].cpu().numpy().transpose((2, 0, 1)) for x in trajectory], os.path.join(out_dir, f'trajectory_{id}.gif'))
+  save_as_gif([x[0].cpu().numpy() for x in trajectory], os.path.join(out_dir, f'trajectory_{id}.gif'))
