96404af6bd361a5c3842b6e82b0dae53a1da423d
bilkitty <githinjibcw@hotmail.com>
2023-08-14 14:54:54 -0400

    fix path

--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 export PYTHONPATH="$HOME/mj_envs_vision/:$PYTHONPATH"
-export PYTHONPATH="$HOME/dependencies/PlaNet/:$PYTHONPATH"
+export PYTHONPATH="$HOME/mj_envs_vision/dependencies/PlaNet/:$PYTHONPATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin
 # NOTE: if rendering offscreen, comment this line or use 'unset LD_PRELOAD'.
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so:/usr/lib/x86_64-linux-gnu/libGL.so
